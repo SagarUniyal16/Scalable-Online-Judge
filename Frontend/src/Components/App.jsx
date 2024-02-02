2a6691647f82061ff0c1086378cd100eb4d280b6
SagarUniyal16 <uniyalsagar211@gmail.com>
2024-02-02 22:20:05 +0530

    Create Navbar and Login Design

---
+++ Frontend/src/Components/App.jsx
@@ -0,0 +1,14 @@
+import Login from "./Login"
+import Header from "./Header"
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return(
+    <div>
+      <Header />
+      <Login />
+
+    </div>
+  )
+}
+
+export default App
---
+++ Frontend/src/Components/Login.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "../../public/Login.css";
+export default function Login() {
+  return (
+    <div className="form-container">
+      <form action="/login" method="post">
+        <div className="login-form-container">
+          <div className="mb-4 ">
+            <label>Username<br />
+            <input type="text" className="box-border mt-2" placeholder="Enter username" />
+            </label>
+
+          </div>
+          <div className="mb-4">
+            <label>Password<br />
+            <input type="password"  className="box-border mt-2" placeholder="Enter password" />
+            </label>
+            </div>
+            <div>
+                <input type="button" className="btn btn-primary" value="login" />
+            </div>
+        </div>
+      </form>
+    </div>
+  );
+}
