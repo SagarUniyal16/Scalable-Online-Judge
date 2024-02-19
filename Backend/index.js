dbf24a7c9fd541c0333dbb5cb8ebff6367ab0a2d
SagarUniyal16 <uniyalsagar211@gmail.com>
2024-02-20 02:18:08 +0530

    Added User Signup Functionality to backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,15 +1,29 @@
+import cors from 'cors';
 import express from "express";
-import bodyParser from "body-parser";
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
+
+import loginRouter from "./Routes/login.js"
+import signupRouter from "./Routes/Signup.js";
+
+
+dotenv.config();
+const port = process.env.PORT || 3000; // Default to port 3000 if PORT is not set in the environment
+
 
 const app = express();
-const port = 3000;
+app.use(express.json());
+app.use(cors());
 
-app.use(bodyParser.urlencoded({extended : true}));
+mongoose.connect(process.env.MONGO_URL);
 
-app.get("/",(req,res)=>{
-    res.send("<h1> Hello </h1>");
-});
+app.use("/signup",signupRouter);
 
-app.listen(port,()=>{
-    console.log(`server running at port ${port}`);
+app.use("/login",loginRouter);
+
+
+
+app.listen(port, () => {
+    console.log("Servaer listining on Port ");
+
 });
