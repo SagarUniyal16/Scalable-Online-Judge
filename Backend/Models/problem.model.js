afd5f9f78db2c4a7dbddbfebb0689f8e21a7e494
SagarUniyal16 <uniyalsagar211@gmail.com>
2024-02-19 23:38:43 +0530

    Created database schema for User and Problems

---
+++ Backend/Models/problem.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL);
+
+const ProblemSchema = new mongoose.Schema({
+  Name: { type: String, required: true },
+  Statement: { type: String, required: true },
+  Difficulty: { type: String, required: true },
+  Code: { type: String, required: true },
+},{
+  timestamps:true,
+});
+
+const Problem = mongoose.model("Problem", ProblemSchema);
+export default Problem;
---
+++ Backend/Models/user.model.js
@@ -0,0 +1,13 @@
+
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name : {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true}
+})
+
+const User = mongoose.model('Users', userSchema);
+export default User;
